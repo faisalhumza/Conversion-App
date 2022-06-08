01ed8efeb251f6d8bab89594b68cbaeb19006dcf
faisalhumza132 <90932452+faisalhumza132@users.noreply.github.com>
2022-06-07 21:34:59 -0400

    Creating the Main Activity

--- app/src/main/java/com/example/conversionapp/MainActivity.java
+++ app/src/main/java/com/example/conversionapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.conversionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView title;
+    TextView auth;
+    TextView category;
+    EditText category1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        title = (TextView) findViewById(R.id.title);
+        auth = (TextView) findViewById(R.id.auth);
+        category = (TextView) findViewById(R.id.category);
+        category1 = (EditText) findViewById(R.id.category1);
     }
 }
